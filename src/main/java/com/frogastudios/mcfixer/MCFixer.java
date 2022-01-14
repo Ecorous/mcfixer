ddb7836d99d2fd596f7e37cf44809567a18ca27d
ExoPlant <lennyj2468+la@gmail.com>
2022-01-14 20:01:32 +0000

    Initial Commit

---
+++ src/main/java/com/frogastudios/mcfixer/MCFixer.java
@@ -0,0 +1,10 @@
+package com.frogastudios.mcfixer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MCFixer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/frogastudios/mcfixer/client/MCFixerClient.java
@@ -0,0 +1,13 @@
+package com.frogastudios.mcfixer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MCFixerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
